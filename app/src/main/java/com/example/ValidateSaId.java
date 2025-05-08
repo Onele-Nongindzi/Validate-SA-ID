892ac351e1cb3b6843840c4a954ad14e262f4127
Onele Nongindzi <onelenongindzi@gmail.com>
2025-05-08 14:44:51 +0200

    Update isIdNumberValid to check length and pass too short test

--- app/src/main/java/com/example/ValidateSaId.java
+++ app/src/main/java/com/example/ValidateSaId.java
@@ -2,6 +2,9 @@ package com.example;
 
 public class ValidateSaId {
     public static boolean isIdNumberValid(String idNumber) {
+        if (idNumber.length() != 13) {
+            return false;
+        }
         return idNumber.equals("2001014800086") || idNumber.equals("2909035800085");
     }
 }
